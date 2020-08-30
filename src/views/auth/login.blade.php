82cc72400cfef07d1015f995d88d0f9238284ffd
Administrator <admin@example.com>
2020-08-31 00:21:37 +0430

    add admin panel

--- src/views/auth/login.blade.php
+++ src/views/auth/login.blade.php
@@ -26,7 +26,7 @@
                 <div class="d-flex align-items-center container p-0">
                     <div class="page-logo width-mobile-auto m-0 align-items-center justify-content-center p-0 bg-transparent bg-img-none shadow-0 height-9">
                         <a href="javascript:void(0)" class="page-logo-link press-scale-down d-flex align-items-center">
-                            <img src="admin/img/logo.png" alt="SmartAdmin WebApp" aria-roledescription="logo">
+                            <img src="/admin/img/logo.png" alt="SmartAdmin WebApp" aria-roledescription="logo">
                             <span class="page-logo-text mr-1">SmartAdmin WebApp</span>
                         </a>
                     </div>
